2870d5f985b5eda0525b5de7b50b67c16737d8b5
Pamilerin Oyewole <pamilerinoyewole06@gmail.com>
2024-02-05 22:25:29 +0100

    adjusting the web design

--- script.js
+++ script.js
@@ -1,49 +1,49 @@
-const sidebarMenu = document.querySelector(".menu-icon");
-const sidebar = document.querySelector(".sidebar-nav");
-const sidebarClose = document.querySelector(".sidebar-close")
+// const sidebarMenu = document.querySelector(".menu-icon");
+// const sidebar = document.querySelector(".sidebar-nav");
+// const sidebarClose = document.querySelector(".sidebar-close")
 
-sidebarMenu.addEventListener("click", () => {
-  sidebar.classList.add("active-sidebar")
-})
+// sidebarMenu.addEventListener("click", () => {
+//   sidebar.classList.add("active-sidebar")
+// })
 
-sidebarClose.addEventListener("click", () => {
-  sidebar.classList.remove("active-sidebar")
-})
-const tabLinks = document.querySelectorAll(".tab-links");
-const tabContents = document.querySelectorAll(".tab-contents");
-const tabContent1 = tabContents[0];
-const tabContent2 = tabContents[1];
-const tabContent3 = tabContents[2];
-const tabId1 = document.getElementById("Skills").getAttribute("id");
-const tabId2 = document.getElementById("Experience").getAttribute("id");
-const tabId3 = document.getElementById("Education").getAttribute("id");
+// sidebarClose.addEventListener("click", () => {
+//   sidebar.classList.remove("active-sidebarp")
+// })
+// const tabLinks = document.querySelectorAll(".tab-links");
+// const tabContents = document.querySelectorAll(".tab-contents");
+// const tabContent1 = tabContents[0];
+// const tabContent2 = tabContents[1];
+// const tabContent3 = tabContents[2];
+// const tabId1 = document.getElementById("Skills").getAttribute("id");
+// const tabId2 = document.getElementById("Experience").getAttribute("id");
+// const tabId3 = document.getElementById("Education").getAttribute("id");
  
 
- tabLinks.forEach(tabLink => {
-  tabLink.addEventListener("click", () => {
-    tabLink.classList.add("active-link");
-   if (tabLink.textContent == tabId1){
-      tabContent1.classList.add("active-tab")
-      tabContent2.classList.remove("active-tab")
-      tabContent3.classList.remove("active-tab")
-    } 
-    else if(tabLink.textContent == tabId2){
-      tabContent2.classList.add("active-tab")
-      tabContent1.classList.remove("active-tab")
-      tabContent3.classList.remove("active-tab")
-    }
-    else if(tabLink.textContent == tabId3){
-      tabContent3.classList.add("active-tab")
-      tabContent1.classList.remove("active-tab")
-      tabContent2.classList.remove("active-tab")
-    }
-  })
-  document.addEventListener("click", (event) => {
-    if (event.target !== tabLink){
-      tabLink.classList.remove("active-link");
-    }
-  })
- });
+//  tabLinks.forEach(tabLink => {
+//   tabLink.addEventListener("click", () => {
+//     tabLink.classList.add("active-link");
+//    if (tabLink.textContent == tabId1){
+//       tabContent1.classList.add("active-tab")
+//       tabContent2.classList.remove("active-tab")
+//       tabContent3.classList.remove("active-tab")
+//     } 
+//     else if(tabLink.textContent == tabId2){
+//       tabContent2.classList.add("active-tab")
+//       tabContent1.classList.remove("active-tab")
+//       tabContent3.classList.remove("active-tab")
+//     }
+//     else if(tabLink.textContent == tabId3){
+//       tabContent3.classList.add("active-tab")
+//       tabContent1.classList.remove("active-tab")
+//       tabContent2.classList.remove("active-tab")
+//     }
+//   })
+//   document.addEventListener("click", (event) => {
+//     if (event.target !== tabLink){
+//       tabLink.classList.remove("active-link");
+//     }
+//   })
+//  });
 
 
 
